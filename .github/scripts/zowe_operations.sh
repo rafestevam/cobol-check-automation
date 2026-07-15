8f7756c52a7263c8abb6ad90fd8e2419a0d5e81b
Rafael Estevam <rafestevam@gmail.com>
2026-07-15 18:29:23 -0300

    Configuring zowe profiles connection

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -3,6 +3,33 @@
 # Convert username to lowercase
 LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME"| tr '[:upper:]' '[:lower:]')
 
+# ZOWE CLI Profile init
+zowe config init --global-config --no-prompt
+
+# ZOWE CLI Profile setting (ZOSMF)
+zowe config set profiles.zosmf.type            "zosmf"      --global-config
+zowe config set profiles.zosmf.properties.host "$ZOWE_HOST" --global-config
+zowe config set profiles.zosmf.properties.port "$ZOWE_PORT" --global-config
+
+# ZOWE CLI Profile setting (Global Base)
+zowe config set profiles.global_base.type                           "base"           --global-config
+zowe config set profiles.global_base.properties.host                "$ZOWE_HOST"     --global-config
+zowe config set profiles.global_base.properties.rejectUnauthorized  "false"          --global-config
+zowe config set profiles.global_base.properties.user                "$ZOWE_USERNAME" --global-config --secure
+zowe config set profiles.global_base.properties.password            "$ZOWE_PASSWORD" --global-config --secure
+
+# ZOWE CLI Profile setting (Bind to Defaults)
+zowe config set defaults.zosmf  "zosmf"       --global-config
+zowe config set defaults.base   "global_base" --global-config
+
+# Validate ZOSMF connection
+echo "Testing z/OSMF connection..."
+if ! zowe zosmf check status; then
+    echo "ERROR: Could not connect to z/OSMF. Aborting."
+    exit 1
+fi
+echo "Connection OK."
+
 # Check if directory exists, create if does not
 if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
     echo "Directory does not exist. Creating it..."
